390301ac887c558e738a43ee01a07ccfb939b645
Bojan Markovic <bmarkovic.79@gmail.com>
2015-11-21 12:12:23 +0100

    Included test for matrix change in both matrix implementation and test file

--- cachematrix.R
+++ cachematrix.R
@@ -6,16 +6,26 @@
 ## and ability to store/retreive cached copy of matrice's inverse
 makeCacheMatrix <- function(x = matrix()) {
   
-  inv <- NULL
+  inv <- NULL ## for caching inverse
+  old <- x    ## caches matrix for comparison
   set <- function (y) {
+    old <<- x
     x <<- y
     inv <<- NULL
   }
   get <- function() x
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
-  
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  setold <- function(inverse) old <<- old
+  getold <- function() old
+  ## safer matrix comparison by Rui Barradas retrieved from:
+  ## https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html
+  unchanged <- function() dim(x) == dim(old) && all(x == old)
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       setold = setold, getold = getold, 
+       getinverse = getinverse, 
+       unchanged = unchanged)
 
 }
 
@@ -26,11 +36,15 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
   inv <- x$getinverse()
+
   if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
+    if(x$unchanged()) { 
+      message("getting cached data")
+      return(inv)
+    }
   }
   data <- x$get()
+  x$setold(data)
   inv <- solve(data, ...)
-  
+  x$setinverse(inv)
 }
